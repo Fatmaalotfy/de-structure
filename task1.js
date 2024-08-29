37bd65efed509f468db587f95336cf8fdd7008c7
fatma lotfy <fatma.lotfy292@gmail.com>
2024-08-29 04:44:38 +0300

    task1

---
+++ task1.js
@@ -0,0 +1,12 @@
+const array=[
+8,
+"55",
+[2,"hello world", {a:2,b:5,},false,],
+{
+arr:[true,1,NaN,new Array(2,33)],
+test:null,
+obj:{d:"Moha",last:[2,false,undefined]},
+},
+];
+const[ , , ,{arr:[ , , ,[ ,second]],obj:{d:text}}]=array;
+console .log (second,text);
